3b718f1716ec564a2f1fa4941b8159e089a0d4be
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-06-26 03:00:33 +0200

    make as PWA

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,7 +2,7 @@ module.exports = {
   ci: {
     collect: {
       numberOfRuns: 3,
-      staticDistDir: "./dist/ng-github-lighthouse",
+      staticDistDir: "./dist/ng-github-lighthouse/browser",
     },
     assert: {
       assertions: {

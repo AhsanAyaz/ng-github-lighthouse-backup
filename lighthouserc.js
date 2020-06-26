306af13b53d1db2d180880a589592c682d51b49f
Muhammad Ahsan Ayaz <ahsan.ubitian@gmail.com>
2020-06-26 02:16:59 +0200

    added lighthouse config

---
+++ lighthouserc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 3,
+      staticDistDir: "./dist/ng-github-lighthouse",
+    },
+    assert: {
+      assertions: {
+        "first-contentful-paint": ["warn", { maxNumericValue: 4000 }],
+        "categories:performance": ["warn", { minScore: 0.9 }],
+        "categories:accessibility": ["error", { minScore: 0.9 }],
+        "categories:seo": ["error", { minScore: 1 }],
+        "installable-manifest": "on",
+        "offline-start-url": "on",
+        "service-worker": "on",
+        "works-offline": "on",
+      },
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
